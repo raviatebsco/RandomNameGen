924f4cf82f785c36b2235d94486d14c60491cc1a
rchandrasekaran <rchandrasekaran@ebsco.com>
2016-05-18 10:50:47 -0400

    Initial

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var aNames = new string[] {
+				"X",
+				"7",
+				"Zeppelin",
+				"Crazy Legs",
+				"Sliver",
+				"Build-R-Us",
+				"The Gang",
+				"Pro Pirates",
+				"Accolades",
+				"Brainy Buddies",
+				"Bosco"
+			};
+
+			Console.WriteLine(string.Format("And the Winner is : {0}!!!", aNames[(new Random()).Next(0, aNames.Count() - 1)]));
+			Console.ReadLine();
+		}
+	}
+}
